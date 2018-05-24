c9fdfd48e1c8aa5085978f65a09b2393bb005bd2
HalfMillennium <glc2266@gmail.com>
2018-05-23 20:09:24 -0400

    v0.2
    Neither of these laptops are super reliable, so, yeah.

--- Bava-Detector/bava_detector.py
+++ Bava-Detector/bava_detector.py
@@ -2,15 +2,39 @@ import numpy as np
 import cv2
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascades/haarcascade_frontalface_alt2.xml')
+prof_cascade = cv2.CascadeClassifier('cascades/data/haarcascades/haarcascade_profileface.xml')
+smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascades/haarcascade_smile.xml')
 
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainer.yml")
+
+#eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascades/haarcascade_eye.xml')
 cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=2.0, minNeighbors=5)
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=2, minNeighbors=5)
+    #eyes = eye_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    profiles = prof_cascade.detectMultiScale(gray, scaleFactor=2, minNeighbors=5)
+    smiles = smile_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
     for(x, y, w, h) in faces:
+        #print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+
+        id_, conf = recognizer.predict(roi_gray)
+        if conf >= 45 and conf <= 90:
+            print(id_)
+        color = (255, 0, 0) #BGR
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+
+    for(x, y, w, h) in profiles:
         print(x,y,w,h)
         roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
 
@@ -18,7 +42,17 @@ while(True):
         stroke = 2
         end_cord_x = x + w
         end_cord_y = y + h
-        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke) 
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+
+    """for(x, y, w, h) in eyes:
+        print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+
+        color = (255, 0, 0) #BGR
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)"""
     # Display the resulting frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
--- Bava-Detector/faces_train.py
+++ Bava-Detector/faces_train.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import numpy as np
 import cv2
 from PIL import Image
@@ -7,6 +8,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascades/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 current_id = 0
 label_ids = {}
@@ -18,18 +20,18 @@ for root, dirs, files in os.walk(image_dir):
         if file.endswith("png") or file.endswith("jpg"):
             path = os.path.join(root, file)
             label = os.path.basename(os.path.dirname(path)).replace(" ", "-").lower()
-            print(label, path)
+            #print(label, path)
             if not label in label_ids:
                 label_ids[label] = current_id
                 current_id += 1
 
             id_ = label_ids[label]
-            print(label_ids)
+            #print(label_ids)
             # y_labels.append(label) # some number
             # x_train.append(path) # verify this image, turn into a NUMPY array, GRAY
             pil_image = Image.open(path).convert("L") # grayscale
             image_array = np.array(pil_image, "uint8")
-            print(image_array)
+            #print(image_array)
             faces = face_cascade.detectMultiScale(image_array, scaleFactor=2, minNeighbors=5)
 
             for (x,y,w,h) in faces:
@@ -37,5 +39,11 @@ for root, dirs, files in os.walk(image_dir):
                 x_train.append(roi)
                 y_labels.append(id_)
 
-print(y_labels)
-print(x_train)
+#print(y_labels)
+#print(x_train)
+
+with open("labels.pickle", 'wb') as f:
+    pickle.dump(label_ids, f)
+
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save("trainer.yml")
